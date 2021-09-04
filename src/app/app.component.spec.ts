67b9d685e67ad95fe6e6608b1ad7401e42fb31c2
Dhanya <dhanu.nova@gmail.com>
2021-09-04 19:28:48 +1000

    Added new test cases for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'weather'`, () => {
+  it(`should have as text 'Dhanya'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('weather');
+    expect(app.weatherapp).toEqual('Dhanya');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('weather app is running!');
+    expect(compiled.querySelector('.container h1')?.textContent).toContain('Hello Dhanya');
   });
 });
